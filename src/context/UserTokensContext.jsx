bc6fccaf6d17129b9d8955abb72e8123e880b4b8
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-06 10:11:52 +0330

    Save Tokens

--- src/context/UserTokensContext.jsx
+++ src/context/UserTokensContext.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const UserTokensContext = createContext();
 
-export default function UserTokensProvider({ children }) {
-  const [tokens, setTokens] = useState(0);
+export const UserTokensProvider = ({ children }) => {
+  const [tokens, setTokens] = useState(() => {
+    const savedTokens = localStorage.getItem("tokens");
+    return savedTokens ? parseInt(savedTokens, 10) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tokens", tokens);
+  }, [tokens]);
 
   return <UserTokensContext.Provider value={{ tokens, setTokens }}>{children}</UserTokensContext.Provider>;
-}
+};

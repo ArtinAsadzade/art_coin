2cb1767ffcfa26b2394914278beb3753cb52eb96
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-06 10:11:45 +0330

    Save Tokens

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { useNavigate, useRoutes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import { routers } from "./router/routes";
-import { useContext, useEffect } from "react";
-import { IsMobileViewContext } from "./context/IsMobileViewContext";
-import UserTokensProvider from "./context/UserTokensContext";
+import { UserTokensProvider } from "./context/UserTokensContext";
 
 function App() {
   const routes = useRoutes(routers);
-  const navigate = useNavigate();
-  const { setIsMobile } = useContext(IsMobileViewContext);
-
-  useEffect(() => {
-    const isDesktop = window.matchMedia("(min-width: 1024px)").matches;
-    if (isDesktop) {
-      navigate("/JustMobileView");
-      setIsMobile(false);
-    }
-  }, [navigate, setIsMobile]);
 
   return (
     <>

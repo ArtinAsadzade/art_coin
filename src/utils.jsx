4861bcd4fd4d62569f47e4c2f2b99b622c45bc2c
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-06 09:46:52 +0330

    Update

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,12 +1,1 @@
-import { useContext } from "react";
-import { UserTokensContext } from "./context/UserTokensContext";
 
-export const useTapBilakhCoin = () => {
-  const { setTokens } = useContext(UserTokensContext);
-
-  const tapBilakhCoin = () => {
-    setTokens((prevTokens) => prevTokens + 1);
-  };
-
-  return tapBilakhCoin;
-};

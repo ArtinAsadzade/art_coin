c5649189ada5a277df15b045054c6c0276c434b7
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-07 10:56:47 +0330

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,3 @@
 export default function Footer() {
-  return <div className="absolute bottom-0 bg-secondary w-full p-2 text-primary font-bold text-center">Owned By Artin Asadzade</div>;
+  return <div className="absolute bottom-0 bg-secondary w-full p-2 text-primary font-bold text-center">Art Coin Powered By @http.artin</div>;
 }

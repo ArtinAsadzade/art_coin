0b0931421cba9c9fe2cc9a80f44cd9c5e04934e8
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-07 10:56:28 +0330

    Fix Android View

--- src/pages/JustMobileView.jsx
+++ src/pages/JustMobileView.jsx
@@ -12,7 +12,7 @@ export default function JustMobileView() {
         <>
           <div className="w-full h-svh bg-primary text-secondary flex items-center justify-center">
             <div className="text-center p-6 bg-white border border-gray-300 rounded-lg shadow-lg">
-              <h1 className="text-2xl font-bold text-gray-800 mb-4">Access Restricted</h1>
+              <h1 className="text-2xl font-bold text-secondary mb-4">Access Restricted</h1>
               <p className="text-lg mb-2">Sorry, our website is not accessible on desktop computers.</p>
               <p className="text-lg">Please visit us again using your mobile device.</p>
             </div>

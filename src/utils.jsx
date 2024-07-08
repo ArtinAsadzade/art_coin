52dc5338daa4c127acdcd6a6d84798b479c85ad2
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-08 13:06:17 +0330

    Update

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,1 +1,13 @@
+import { useState } from "react";
+import { ranksData } from "./data/Data";
 
+export const findRank = (tokens) => {
+  const [ranks] = useState(ranksData);
+
+  for (let rank of ranks) {
+    if (tokens >= rank.start && tokens <= rank.end) {
+      return rank.title;
+    }
+  }
+  return null;
+};

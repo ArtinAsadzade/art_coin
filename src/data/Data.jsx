20d625bcd5a82cce9c85cef88503f5d637e36c19
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-08 13:06:23 +0330

    Update

---
+++ src/data/Data.jsx
@@ -0,0 +1,18 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+export const bannedOSListData = ["Mac OS", "Linux", "Windows"];
+
+export const navData = [
+  { id: 1, title: "Rer", icon: <XMarkIcon className="w-8" />, func: "" },
+  { id: 2, title: "Task", icon: <XMarkIcon className="w-8" />, func: "" },
+  { id: 3, title: "Home", icon: <XMarkIcon className="w-8" />, func: "" },
+  { id: 4, title: "Boost", icon: <XMarkIcon className="w-8" />, func: "" },
+  { id: 5, title: "Status", icon: <XMarkIcon className="w-8" />, func: "" },
+];
+
+export const ranksData = [
+  { id: 1, title: "Gold", start: 0, end: 100 },
+  { id: 2, title: "Silver", start: 101, end: 200 },
+  { id: 3, title: "Diamond", start: 201, end: 300 },
+  { id: 4, title: "Start", start: 301, end: 400 },
+];

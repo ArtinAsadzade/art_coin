d26a0330a4ee754c794577b54de42f20190e7999
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-09 15:35:53 +0330

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import { useRoutes } from "react-router-dom";
 import { routers } from "./router/routes";
 import { UserTokensProvider } from "./context/UserTokensContext";
-import { useEffect } from "react";
 
 function App() {
   const routes = useRoutes(routers);
-
-  useEffect(() => {
-    if (+localStorage.getItem("tokens") > 10000) {
-      localStorage.clear();
-      setTimeout(() => {
-        location.reload();
-      }, 2000);
-    }
-  }, []);
 
   return (
     <>

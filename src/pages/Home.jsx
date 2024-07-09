2dfd40852b055a15064c5e192fa6db868619be8b
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-09 10:44:10 +0330

    UpdateUpdate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,17 @@
-import { useContext } from "react";
-import { IsMobileViewContext } from "../context/IsMobileViewContext";
-import { Navigate } from "react-router-dom";
 import Coin from "../components/Coin";
 import Footer from "../components/Footer";
 import NavContainer from "../components/Nav/NavContainer";
 import Tokens from "../components/Tokens";
 
 export default function Home() {
-  const { isMobile } = useContext(IsMobileViewContext);
-
   return (
     <>
-      {isMobile ? (
-        <div className="w-full h-svh flex flex-col items-center p-5 bg-primary relative select-none">
-          <Tokens />
-          <Coin />
-          <NavContainer />
-          <Footer />
-        </div>
-      ) : (
-        <Navigate to={"/JustMobileView"} />
-      )}
+      <div className="w-full h-svh flex flex-col items-center p-5 bg-primary relative select-none">
+        <Tokens />
+        <Coin />
+        <NavContainer />
+        <Footer />
+      </div>
     </>
   );
 }

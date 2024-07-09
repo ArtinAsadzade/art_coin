cd7dabd0f6cc2f15980f21d5a7cc47f20095b332
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-09 10:44:04 +0330

    Update

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -11,25 +11,20 @@ export default function ComingSoon() {
 
   return (
     <>
-      {isMobile ? (
-        <div className="w-full h-svh flex flex-col items-center p-5 bg-primary relative select-none">
-          <div className="bg-secondary py-2 w-1/2 text-center rounded-lg mt-10 text-2xl text-primary font-bold flex justify-between px-5">
-            <p>{tokens}</p>
-            <p>Coin</p>
-          </div>
-          <Coin />
-          <div className="font-bold text-secondary">
-            <h1 className="text-2xl">Art Coin Coming Soon!</h1>
-            <p>
-              Exciting news! Art Coin is launching soon – a fun and engaging airdrop project where you can win Art Coins and enjoy unique
-              opportunities.
-            </p>
-          </div>
-          <Footer />
+      <div className="w-full h-svh flex flex-col items-center p-5 bg-primary relative select-none">
+        <div className="bg-secondary py-2 w-1/2 text-center rounded-lg mt-10 text-2xl text-primary font-bold flex justify-between px-5">
+          <p>{tokens}</p>
+          <p>Coin</p>
         </div>
-      ) : (
-        <Navigate to={"/JustMobileView"} />
-      )}
+        <Coin />
+        <div className="font-bold text-secondary">
+          <h1 className="text-2xl">Art Coin Coming Soon!</h1>
+          <p>
+            Exciting news! Art Coin is launching soon – a fun and engaging airdrop project where you can win Art Coins and enjoy unique opportunities.
+          </p>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }

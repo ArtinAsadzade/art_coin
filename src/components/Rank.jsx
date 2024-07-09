d75fa544e7bf0969e13dd50324e5542b36ef493f
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-09 15:35:57 +0330

    Update

--- src/components/Rank.jsx
+++ src/components/Rank.jsx
@@ -1,13 +1,12 @@
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import { findRank } from "../utils";
 
 export default function Rank({ tokens }) {
   const rank = findRank(tokens);
 
-  console.log(rank);
-
   return (
-    <div>
-      <div className="">Silder</div>
+    <div className={`text-secondary font-bold flex items-center gap-2 my-3 cursor-pointer`}>
+      {rank.title} <ChevronRightIcon className="w-5" />
     </div>
   );
 }

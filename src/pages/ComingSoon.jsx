f06cdc31fee48f153e92de8d0fb94ec1f7fac86e
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-09 15:36:09 +0330

    Update

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,17 +1,15 @@
 import { useContext } from "react";
-import { IsMobileViewContext } from "../context/IsMobileViewContext";
-import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Coin from "../components/Coin";
 import Footer from "../components/Footer";
 import { UserTokensContext } from "../context/UserTokensContext";
 
 export default function ComingSoon() {
-  const { isMobile } = useContext(IsMobileViewContext);
   const { tokens } = useContext(UserTokensContext);
 
   return (
     <>
-      <div className="w-full h-svh flex flex-col items-center p-5 bg-primary relative select-none">
+      <div className="w-full h-svh flex flex-col items-center justify-between p-5 bg-primary relative select-none">
         <div className="bg-secondary py-2 w-1/2 text-center rounded-lg mt-10 text-2xl text-primary font-bold flex justify-between px-5">
           <p>{tokens}</p>
           <p>Coin</p>
@@ -23,6 +21,9 @@ export default function ComingSoon() {
             Exciting news! Art Coin is launching soon – a fun and engaging airdrop project where you can win Art Coins and enjoy unique opportunities.
           </p>
         </div>
+        <Link to={"/login"} className="w-full bg-secondary rounded-lg p-3 text-primary font-bold text-center cursor-pointer">
+          what is happening?
+        </Link>
         <Footer />
       </div>
     </>

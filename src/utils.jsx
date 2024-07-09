bbcc7d414e28d06c67018f466cccd0f4a5a265a0
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-09 15:36:11 +0330

    Update

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,25 +1,40 @@
 import { useState } from "react";
 import { ranksData } from "./data/Data";
+import CryptoJS from "crypto-js";
+
+export const encrypted = (data, key) => {
+  const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), import.meta.env.VITE_SECRET_KEY).toString();
+  localStorage.setItem(key, encryptedData);
+};
+
+export const decrypted = (key) => {
+  const encryptedData = localStorage.getItem(key);
+  if (encryptedData) {
+    try {
+      const bytes = CryptoJS.AES.decrypt(encryptedData, import.meta.env.VITE_SECRET_KEY);
+      return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    } catch (error) {
+      localStorage.clear();
+      location.reload();
+      return "";
+    }
+  } else return "";
+};
 
 export const findRank = (tokens) => {
   const [ranks] = useState(ranksData);
 
   for (let rank of ranks) {
     if (tokens >= rank.start && tokens <= rank.end) {
-      return rank.title;
+      return rank;
     }
   }
   return null;
 };
 
-export const findUserEmail = (email, users) => {
-  const user = users.find((user) => user.email === email);
-  return user ? user.email : "";
-};
-
-export const findUserCode = (code, users) => {
-  const user = users.find((user) => user.code === code);
-  return user ? user.code : "";
+export const findUserHandler = (email, code, users) => {
+  const user = users.find((user) => user.email === email && user.code === code);
+  return user ? user : "";
 };
 
 export const getPinAsNumber = (pin) => {

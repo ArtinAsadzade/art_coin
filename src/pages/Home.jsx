d114a9b9b19c44c7a9fe3073a2b13e011a4f6e6f
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-14 13:31:14 +0330

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import Coin from "../components/Coin";
-import NavContainer from "../components/Nav/NavContainer";
-import Tokens from "../components/Tokens";
+// import Coin from "../components/Coin";
+// import NavContainer from "../components/Nav/NavContainer";
+// import Tokens from "../components/Tokens";
 
-export default function Home() {
-  return (
-    <>
-      <div className="w-full h-svh flex flex-col items-center p-5 bg-primary relative select-none">
-        <Tokens />
-        <Coin />
-        <NavContainer />
-      </div>
-    </>
-  );
-}
+// export default function Home() {
+//   return (
+//     <>
+//       <div className="w-full h-svh flex flex-col items-center p-5 bg-primary relative select-none">
+//         <Tokens />
+//         <Coin />
+//         <NavContainer />
+//       </div>
+//     </>
+//   );
+// }

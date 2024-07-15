8162f73e5842abdd2e396c46fe2e8e3415983872
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-15 22:53:45 +0330

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useRoutes } from "react-router-dom";
 import { routers } from "./router/routes";
 import { UserTokensProvider } from "./context/UserTokensContext";
+import { useEffect } from "react";
+import { getUserData } from "./utils";
 
 function App() {
   const routes = useRoutes(routers);
+
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   return (
     <>

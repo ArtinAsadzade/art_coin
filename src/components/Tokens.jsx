40f3e9c3f8e5a6c593a9d1d3501d3aa3e6ac6b34
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-15 23:36:29 +0330

    Update

--- src/components/Tokens.jsx
+++ src/components/Tokens.jsx
@@ -1,16 +1,17 @@
 import { useContext } from "react";
 import Rank from "./Rank";
 import { UserTokensContext } from "../context/UserTokensContext";
+import { formatNumber } from "../utils";
 
 export default function Tokens() {
   const { tokens } = useContext(UserTokensContext);
   return (
-    <>
-      <div className="bg-secondary py-2 w-1/2 text-center rounded-lg mt-10 text-2xl text-primary font-bold flex justify-between px-5">
-        <p>{tokens}</p>
-        <p>Coin</p>
+    <div className="w-full flex flex-col items-center">
+      <div className="py-2 text-center rounded-lg text-5xl text-secondary font-bold flex items-center justify-center gap-3">
+        <img className="w-[50px] mt-3 object-cover" src="/logo.webp" alt="" />
+        <p>{formatNumber(tokens)}</p>
       </div>
       <Rank tokens={tokens} />
-    </>
+    </div>
   );
 }

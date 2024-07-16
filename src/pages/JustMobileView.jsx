3e79855b064da82614f5649fa318c4d426980ca4
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-16 22:09:30 +0330

    Fix

--- src/pages/JustMobileView.jsx
+++ src/pages/JustMobileView.jsx
@@ -1,13 +1,22 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { IsMobileViewContext } from "../context/IsMobileViewContext";
+
 export default function JustMobileView() {
+  const { isMobile } = useContext(IsMobileViewContext);
   return (
     <>
-      <div className="w-full h-svh bg-primary text-secondary flex items-center justify-center">
-        <div className="text-center p-6 bg-white border border-gray-300 rounded-lg shadow-lg">
-          <h1 className="text-2xl font-bold text-secondary mb-4">Access Restricted</h1>
-          <p className="text-lg mb-2">Sorry, our website is not accessible on desktop computers.</p>
-          <p className="text-lg">Please visit us again using your mobile device.</p>
+      {isMobile ? (
+        <Navigate to={"/"} />
+      ) : (
+        <div className="w-full h-svh bg-primary text-secondary flex items-center justify-center">
+          <div className="text-center p-6 bg-white border border-gray-300 rounded-lg shadow-lg">
+            <h1 className="text-2xl font-bold text-secondary mb-4">Access Restricted</h1>
+            <p className="text-lg mb-2">Sorry, our website is not accessible on desktop computers.</p>
+            <p className="text-lg">Please visit us again using your mobile device.</p>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }

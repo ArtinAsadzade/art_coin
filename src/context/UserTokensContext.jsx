6e260e44904a2514b31a5567bc372999e5283813
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-01 19:55:12 +0330

    Update

--- src/context/UserTokensContext.jsx
+++ src/context/UserTokensContext.jsx
@@ -1,13 +1,9 @@
 import { createContext, useState } from "react";
-import { decrypted } from "../utils";
 
 export const UserTokensContext = createContext();
 
 export const UserTokensProvider = ({ children }) => {
-  const [tokens, setTokens] = useState(() => {
-    const data = decrypted("account");
-    return data && data.email ? parseInt(data.coins, 10) || 0 : 0;
-  });
+  const [tokens, setTokens] = useState(0);
 
   return <UserTokensContext.Provider value={{ tokens, setTokens }}>{children}</UserTokensContext.Provider>;
 };

c1f9c37c4697a4f066f076486e5cf6ffbfa8711e
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-01 19:55:23 +0330

    Update

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,28 +1,28 @@
+/* eslint-disable no-unused-vars */
 import { Outlet, useNavigate } from "react-router-dom";
 import { decrypted } from "../utils";
 import { useEffect, useState } from "react";
-import { coming_soon_url, login_url } from "./Urls";
+import { home_url, login_url } from "./Urls";
 import axios from "axios";
+import Loading from "../components/Loading";
 
 export default function PrivateRoute() {
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState("");
   const navigate = useNavigate();
   const token = decrypted("token");
 
   useEffect(() => {
     if (token) {
-      axios
-        .post(`${import.meta.env.VITE_API}api/users/by-email`, { email: token })
-        .then((res) => {
+      axios.post(`${import.meta.env.VITE_API}api/users/by-email`, { email: token }).then((res) => {
+        if (+res.data.perm > 0) {
           setIsAuthenticated(true);
-        })
-        .catch(() => {
-          navigate(coming_soon_url);
-        });
+          navigate(home_url);
+        }
+      });
     } else {
       navigate(login_url);
     }
   }, [navigate, token]);
 
-  return <>{isAuthenticated ? <Outlet /> : <>Loading..</>}</>;
+  return <>{isAuthenticated ? <Outlet /> : <Loading />}</>;
 }

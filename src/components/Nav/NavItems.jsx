0496c2d1ea22dac8c37a7b7074fb22db71e83262
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-01 19:55:04 +0330

    Update

--- src/components/Nav/NavItems.jsx
+++ src/components/Nav/NavItems.jsx
@@ -4,7 +4,7 @@ export default function NavItems(props) {
   return (
     <Link
       to={props.link}
-      className="w-16 p-2 backdrop:fil bg-secondary bg-opacity-90 font-bold text-primary rounded-md backdrop-blur-sm flex flex-col items-center gap-3 cursor-pointer"
+      className="w-full p-2 bg-secondary bg-opacity-90 font-bold text-primary rounded-md backdrop-blur-sm flex flex-col items-center gap-3 cursor-pointer"
     >
       {props.icon}
       <h3 className="text-sm">{props.title}</h3>

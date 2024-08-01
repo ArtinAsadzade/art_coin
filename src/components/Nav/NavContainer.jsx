cf0a3bafaceeda831aa8798eafa32f4383720a25
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-01 19:55:02 +0330

    Update

--- src/components/Nav/NavContainer.jsx
+++ src/components/Nav/NavContainer.jsx
@@ -1,14 +1,12 @@
 import NavItems from "./NavItems";
 import { useState } from "react";
 import { navData } from "../../data/Data";
-import StatusBar from "./StatusBar";
 
 export default function NavContainer() {
   const [navItems] = useState(navData);
   return (
-    <div className="font-bold w-11/12 rounded-xl absolute bottom-5 gap-8 flex flex-col items-center">
-      <StatusBar />
-      <div className="flex flex-1 justify-center gap-2">
+    <div className="font-bold w-full rounded-xl gap-8 flex flex-col items-center">
+      <div className="w-full flex justify-center gap-2">
         {navItems.map((item) => (
           <NavItems {...item} key={item.id} />
         ))}

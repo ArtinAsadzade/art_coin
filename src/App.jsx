a68a4d7a4307134c7408dce525c4f386221d1d12
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-01 19:54:58 +0330

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import { useRoutes } from "react-router-dom";
+import { useNavigate, useRoutes } from "react-router-dom";
 import { routers } from "./router/routes";
 import { UserTokensProvider } from "./context/UserTokensContext";
 import { useEffect } from "react";
-import { getUserData } from "./utils";
+import { decrypted } from "./utils";
+import axios from "axios";
+import { coming_soon_url, home_url, login_url } from "./router/Urls";
+import { UserAllTokensProvider } from "./context/UserAllTokensContext";
 
 function App() {
   const routes = useRoutes(routers);
+  const navigate = useNavigate();
+  const token = decrypted("token");
 
   useEffect(() => {
-    getUserData();
-  }, []);
+    if (token) {
+      axios
+        .post(`${import.meta.env.VITE_API}api/users/by-email`, { email: token })
+        .then((res) => {
+          if (res.data.perm > 0) {
+            navigate(home_url);
+          } else {
+            navigate(coming_soon_url);
+          }
+        })
+        .catch(() => {
+          navigate(login_url);
+          localStorage.clear();
+        });
+    }
+  }, [navigate, token]);
 
   return (
     <>
-      <UserTokensProvider>{routes}</UserTokensProvider>
+      <UserTokensProvider>
+        <UserAllTokensProvider>{routes}</UserAllTokensProvider>
+      </UserTokensProvider>
     </>
   );
 }

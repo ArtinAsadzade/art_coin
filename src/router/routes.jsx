dde5fe5a5787decc232b2de507d168aabb24deea
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-01 19:54:44 +0330

    Update

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,9 +2,10 @@ import ComingSoon from "../pages/ComingSoon";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import PrivateRoute from "./PrivateRoute";
+import { coming_soon_url, login_url } from "./Urls";
 
 export const routers = [
-  { path: "/", element: <ComingSoon /> },
-  { path: "/login", element: <Login /> },
+  { path: coming_soon_url, element: <ComingSoon /> },
+  { path: login_url, element: <Login /> },
   { path: "/", element: <PrivateRoute />, children: [{ path: "home", element: <Home /> }] },
 ];

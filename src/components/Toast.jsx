6cf4be9e0056187e6ab1c4cbbe8e70f23f6691a1
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-02 12:40:59 +0330

    Update

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -24,16 +24,16 @@ export default function Toast({ icon, msg, show, setShow }) {
   return createPortal(
     <div
       id="toast-default"
-      className={`flex items-center justify-between w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg fixed top-2 right-2 shadow transition-all duration-300 ${
+      className={`flex items-center justify-between w-full max-w-xs p-4 text-gray-500 bg-secondary rounded-lg fixed top-2 right-2 shadow transition-all duration-300 ${
         show ? "opacity-100 visible mr-0" : "opacity-0 invisible animate-slide-out-right -mr-96"
       }`}
       role="alert"
     >
-      <div className="inline-flex items-center justify-center flex-shrink-0 w-10 h-10 bg-slate-200 rounded-lg ">{icon}</div>
-      <div className="ms-3 text-sm font-normal">{msg}</div>
+      <div className="inline-flex items-center justify-center flex-shrink-0 w-10 h-10 bg-primary rounded-lg font-bold">{icon}</div>
+      <div className="ms-3 text-sm font-bold text-primary">{msg}</div>
       <button
         type="button"
-        className="-mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8"
+        className="-mx-1.5 -my-1.5 text-primary rounded-lg inline-flex items-center justify-center h-8 w-8"
         data-dismiss-target="#toast-default"
         aria-label="Close"
         onClick={hideToast}

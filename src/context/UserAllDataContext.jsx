e8c4914b13da376402ba1c51d3e0b2bb5e7423c6
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-02 12:41:04 +0330

    Update

---
+++ src/context/UserAllDataContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+export const UserAllDataContext = createContext();
+
+export const UserAllDataProvider = ({ children }) => {
+  const [allTokens, setAllTokens] = useState(0);
+  const [tokens, setTokens] = useState(0);
+  const [tokenLimit, setTokenLimit] = useState(0);
+
+  return (
+    <UserAllDataContext.Provider value={{ allTokens, setAllTokens, tokens, setTokens, tokenLimit, setTokenLimit }}>
+      {children}
+    </UserAllDataContext.Provider>
+  );
+};

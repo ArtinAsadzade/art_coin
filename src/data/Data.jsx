68e64d4c18deba43339d78e3e5b39608b48c9975
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-02 12:41:06 +0330

    Update

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -11,10 +11,14 @@ export const navData = [
 ];
 
 export const ranksData = [
-  { id: 1, title: "Owner", start: 1000000000000 },
-  { id: 6, title: "Super Star", start: 100000 },
-  { id: 5, title: "Star", start: 400 },
-  { id: 2, title: "Diamond", start: 200 },
-  { id: 3, title: "Silver", start: 100 },
-  { id: 4, title: "Gold", start: 0 },
+  { id: 10, title: "Mythic", start: 1000000000 },
+  { id: 9, title: "Legendary", start: 500000000 },
+  { id: 8, title: "Champion", start: 100000000 },
+  { id: 7, title: "Elite", start: 10000000 },
+  { id: 6, title: "Master", start: 1000000 },
+  { id: 5, title: "Expert", start: 500000 },
+  { id: 4, title: "Professional", start: 100000 },
+  { id: 3, title: "Apprentice", start: 10000 },
+  { id: 2, title: "Beginner", start: 1000 },
+  { id: 1, title: "Novice", start: 0 },
 ];

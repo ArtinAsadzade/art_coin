1007379ca04b828b9792dce29880faa9340271d1
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-04 10:45:34 +0330

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,20 @@
-import { useContext, useEffect } from "react";
 import ClaimTokens from "../components/ClaimTokens";
 import Coin from "../components/Coin";
 import NavContainer from "../components/Nav/NavContainer";
 import StatusBar from "../components/StatusBar";
 import Tokens from "../components/Tokens";
-import { decrypted } from "../utils";
-import axios from "axios";
-import { UserAllDataContext } from "../context/UserAllDataContext";
-import useFetch from "../hooks/useFetch";
-import Loading from "../components/Loading";
-
 export default function Home() {
-  const { loading, setLoading } = useFetch();
-  const { setAllTokens, setTokenLimit, setPerTap } = useContext(UserAllDataContext);
-
-  const email = decrypted("token");
-
-  useEffect(() => {
-    setLoading(true);
-    axios.post(`${import.meta.env.VITE_API}api/users/by-email`, { email }).then((res) => {
-      setAllTokens(+res.data.coins);
-      setTokenLimit(+res.data.coinLimit);
-      setPerTap(+res.data.perTap);
-      setLoading(false);
-    });
-  }, [email, setAllTokens, setLoading, setPerTap, setTokenLimit]);
-
   return (
     <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className="w-full h-screen flex flex-col">
-          <div className="flex-1 flex flex-col gap-5 items-center px-3 bg-primary relative select-none">
-            <Tokens />
-            <StatusBar />
-            <Coin />
-            <ClaimTokens />
-            <NavContainer />
-          </div>
+      <div className="w-full h-screen flex flex-col">
+        <div className="flex-1 flex flex-col gap-5 items-center px-3 bg-primary relative select-none">
+          <Tokens />
+          <StatusBar />
+          <Coin />
+          <ClaimTokens />
+          <NavContainer />
         </div>
-      )}
+      </div>
     </>
   );
 }

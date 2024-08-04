d2941df2905a3ffc5ecde1255b26676d015a8dac
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-04 10:45:37 +0330

    Update

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 import { Outlet, useNavigate } from "react-router-dom";
 import { decrypted } from "../utils";
-import { useEffect, useState } from "react";
-import { coming_soon_url, home_url, login_url } from "./Urls";
+import { useContext, useEffect, useState } from "react";
+import { coming_soon_url, login_url } from "./Urls";
 import axios from "axios";
 import Loading from "../components/Loading";
+import { UserAllDataContext } from "../context/UserAllDataContext";
 
 export default function PrivateRoute() {
   const [isAuthenticated, setIsAuthenticated] = useState("");
+  const { setAllTokens, setTokenLimit, setPerTap } = useContext(UserAllDataContext);
   const navigate = useNavigate();
   const token = decrypted("token");
 
@@ -16,6 +18,9 @@ export default function PrivateRoute() {
       axios.post(`${import.meta.env.VITE_API}api/users/by-email`, { email: token }).then((res) => {
         if (+res.data.perm > 0) {
           setIsAuthenticated(true);
+          setAllTokens(+res.data.coins);
+          setTokenLimit(+res.data.coinLimit);
+          setPerTap(+res.data.perTap);
         } else {
           navigate(coming_soon_url);
         }
@@ -23,7 +28,7 @@ export default function PrivateRoute() {
     } else {
       navigate(login_url);
     }
-  }, [navigate, token]);
+  }, [navigate, setAllTokens, setPerTap, setTokenLimit, token]);
 
   return <>{isAuthenticated ? <Outlet /> : <Loading />}</>;
 }

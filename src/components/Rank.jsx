33a6c510a24300b207fa5f2f7376034e0db927ff
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-04 10:45:27 +0330

    Update

--- src/components/Rank.jsx
+++ src/components/Rank.jsx
@@ -1,6 +1,11 @@
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { findRank } from "../utils";
 
 export default function Rank({ tokens }) {
   const rank = findRank(tokens);
-  return <div>{rank.title}</div>;
+  return (
+    <div className="flex items-center text-bold gap-1">
+      {rank.title} <ArrowRightIcon className="w-5 font-bold mt-1" />
+    </div>
+  );
 }

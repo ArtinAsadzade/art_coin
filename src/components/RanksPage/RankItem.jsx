9accc779b96e4103dca041b463272dbf1c0c1215
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-04 14:22:31 +0330

    Fix

--- src/components/RanksPage/RankItem.jsx
+++ src/components/RanksPage/RankItem.jsx
@@ -3,7 +3,7 @@ import { formatNumber } from "../../utils";
 export default function RankItem(props) {
   return (
     <div className="flex flex-col items-center gap-2 mx-4 font-bold text-secondary py-5 text-2xl">
-      <h1>{props.title} Legue</h1>
+      <h1>{props.title} League</h1>
       <img src={props.src} alt={`${props.title} Rank Picture`} className="w-[400px] h-[300px] object-cover" />
       <p>From {formatNumber(props.start)}</p>
     </div>

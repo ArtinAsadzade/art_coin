15c392f38f56f889614e28b1b3137ea7fc12f5d9
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-14 16:13:03 +0330

    Update

--- src/components/RanksPage/Ranking.jsx/Users.jsx
+++ src/components/RanksPage/Ranking.jsx/Users.jsx
@@ -1,18 +1,16 @@
-import { decrypted, formatNumber } from "../../../utils";
+import { formatNumber } from "../../../utils";
 
 export default function Users({ props, index }) {
-  const email = decrypted("token");
-
   return (
-    <div className="w-full flex justify-between bg-secondary text-primary px-3 py-4 rounded-lg items-center">
-      <div className="flex items-center gap-4">
-        <p>{index + 1} </p>
+    <div className="w-full flex justify-between bg-secondary text-primary px-3 py-1 rounded-lg items-center">
+      <div className="flex flex-col items-start font-extrabold">
         <div className="flex items-center gap-2">
           <h3>{props.name ? props.name : "user_" + props._id.slice(0, 7)}</h3>
-          {props.email === email && <div className="bg-primary text-secondary px-[4px] py-[2px] text-[10px] rounded-md flex items-center">YOU</div>}
+          {/* {props.email === email && <div className="bg-primary text-secondary px-[4px] py-[2px] text-[10px] rounded-md flex items-center">YOU</div>} */}
         </div>
+        <p className="font-bold">{formatNumber(props.coins)}</p>
       </div>
-      <p>{formatNumber(props.coins)}</p>
+      <p className="font-bold text-xl">{index + 1} </p>
     </div>
   );
 }

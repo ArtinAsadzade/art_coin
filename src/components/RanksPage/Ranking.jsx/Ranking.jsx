c4e3cba4b1611ab82cc6f1e16e21787575e9f2b4
ArtinAsadzade <artinasadzade7@gmail.com>
2024-09-08 09:21:28 +0330

    update ranking

--- src/components/RanksPage/Ranking.jsx/Ranking.jsx
+++ src/components/RanksPage/Ranking.jsx/Ranking.jsx
@@ -1,6 +1,10 @@
 import Users from "./Users";
 
-export default function Ranking({ data }) {
+export default function Ranking({ data, userRank }) {
+  const isUserInTop100 = data.some((item) => item.rank === userRank);
+
+  const userSurroundings = data.slice(userRank - 2, userRank + 1);
+
   return (
     <div className="w-full flex flex-col text-secondary font-bold gap-[20px] border-t-[1px] border-secondary pt-5">
       <div className="w-full flex justify-between">
@@ -11,6 +15,17 @@ export default function Ranking({ data }) {
         {data?.slice(0, 100).map((item, index) => (
           <Users key={item.id} index={index} props={item} />
         ))}
+
+        {!isUserInTop100 && (
+          <>
+            <div className="w-full flex justify-between border-t-[1px] border-secondary pt-5">
+              <h5>Your Rank</h5>
+            </div>
+            {userSurroundings.map((item, index) => (
+              <Users key={item.id} index={item.rank} props={item} />
+            ))}
+          </>
+        )}
       </div>
     </div>
   );

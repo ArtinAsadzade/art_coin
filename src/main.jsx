83319ee85458a41e52c2ed9b5aea7840db68c5a0
ArtinAsadzade <artinasadzade7@gmail.com>
2025-05-11 13:57:58 +0330

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import IsMobileViewProvider from "./context/IsMobileViewContext.jsx";
 import { UserAllDataProvider } from "./context/UserAllDataContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(

15d2f43d0c245e8a80f6884b06ae6968389dc21e
ArtinAsadzade <artinasadzade7@gmail.com>
2025-05-12 16:15:36 +0330

    Update

--- src/pages/Tasks/_components/HotTasks.jsx
+++ src/pages/Tasks/_components/HotTasks.jsx
@@ -1,8 +1,8 @@
 import { ClipboardIcon, FireIcon } from "@heroicons/react/24/outline";
 import { useDraggable } from "react-use-draggable-scroll";
 import { useRef } from "react";
-import TaskItem from "./TaskItem";
-import { tasksCategory } from "../../../data/Data";
+// import TaskItem from "./TaskItem";
+// import { tasksCategory } from "../../../data/Data";
 
 export default function HotTasks({ data }) {
   const draggScroll = useRef();
@@ -11,20 +11,20 @@ export default function HotTasks({ data }) {
   return (
     <div>
       <div className="flex flex-col gap-1 py-4">
-        <h2 className="flex items-center text-secondary gap-1 font-extrabold px-5 text-xl">
+        <h2 className="flex items-center text-secondary gap-1 font-extrabold text-xl">
           Hot Tasks <FireIcon className="w-8" />
         </h2>
-        <div className=" flex items-center w-full gap-3 flex-shrink-0 overflow-x-auto px-3 text-center" {...events} ref={draggScroll}>
+        <div className="bg-secondary p-3 text-center font-bold text-xl rounded-lg text-primary" {...events} ref={draggScroll}>
           <p>No task available</p>
           {/* {data?.missions?.slice(0, 5).map((item) => item.missionType === "visit" && <TaskItem item={item} key={item.id} />)} */}
         </div>
       </div>
-      <div>
-        <h2 className="flex items-center text-secondary gap-1 font-extrabold px-5 text-xl">
+      <div className="flex flex-col gap-1 py-4">
+        <h2 className="flex items-center text-secondary gap-1 font-extrabold text-xl">
           Tasks <ClipboardIcon className="w-6" />
         </h2>
-        <div>
-          <p>No task available</p>
+        <div className="bg-secondary p-3 text-center font-bold text-xl rounded-lg text-primary">
+          <p>no task available</p>
           {/* <div className="w-full border-b-[2px] border-secondary pb-1">{tasksCategory.map}</div> */}
         </div>
       </div>

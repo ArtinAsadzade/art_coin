feb7d696d3bf56b6a9b8360959e80e292aba445f
ArtinAsadzade <artinasadzade7@gmail.com>
2025-05-12 16:09:47 +0330

    Update

--- src/pages/Tasks/_components/HotTasks.jsx
+++ src/pages/Tasks/_components/HotTasks.jsx
@@ -14,15 +14,19 @@ export default function HotTasks({ data }) {
         <h2 className="flex items-center text-secondary gap-1 font-extrabold px-5 text-xl">
           Hot Tasks <FireIcon className="w-8" />
         </h2>
-        <div className="flex items-center w-full gap-3 flex-shrink-0 overflow-x-auto px-3" {...events} ref={draggScroll}>
-          {data?.missions?.slice(0, 5).map((item) => item.missionType === "visit" && <TaskItem item={item} key={item.id} />)}
+        <div className=" flex items-center w-full gap-3 flex-shrink-0 overflow-x-auto px-3 text-center" {...events} ref={draggScroll}>
+          <p>No task available</p>
+          {/* {data?.missions?.slice(0, 5).map((item) => item.missionType === "visit" && <TaskItem item={item} key={item.id} />)} */}
         </div>
       </div>
       <div>
         <h2 className="flex items-center text-secondary gap-1 font-extrabold px-5 text-xl">
           Tasks <ClipboardIcon className="w-6" />
         </h2>
-        <div>{/* <div className="w-full border-b-[2px] border-secondary pb-1">{tasksCategory.map}</div> */}</div>
+        <div>
+          <p>No task available</p>
+          {/* <div className="w-full border-b-[2px] border-secondary pb-1">{tasksCategory.map}</div> */}
+        </div>
       </div>
     </div>
   );

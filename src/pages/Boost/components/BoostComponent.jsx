ac666ebea4aac7f479bfec833e837b221df8ff7e
ArtinAsadzade <artinasadzade7@gmail.com>
2025-05-12 16:23:10 +0330

    Update

--- src/pages/Boost/components/BoostComponent.jsx
+++ src/pages/Boost/components/BoostComponent.jsx
@@ -1,3 +1,14 @@
+import { FireIcon } from "@heroicons/react/24/outline";
+
 export default function BoostComponent() {
-  return <div>BoostComponent</div>;
+  return (
+    <div className="w-full flex flex-col gap-4 py-4">
+      <h2 className="flex items-center text-secondary gap-1 font-extrabold text-xl">
+        Upgrades <FireIcon className="w-8" />
+      </h2>
+      <div className="bg-secondary p-3 text-center font-bold text-xl rounded-lg text-primary">
+        <p>No Upgrade available</p>
+      </div>
+    </div>
+  );
 }

d2bfd63c02042959fb000e2e1661f0854bfad5b0
ArtinAsadzade <artinasadzade7@gmail.com>
2025-05-12 16:21:20 +0330

    Update

---
+++ src/pages/Boost/BoostPage.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import Toast from "../../components/Toast";
+import Loading from "../../components/Loading";
+import { decrypted } from "../../utils";
+import useFetch from "../../hooks/useFetch";
+import axios from "axios";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import NavContainer from "../../components/Nav/NavContainer";
+import BoostComponent from "./components/BoostComponent";
+
+export default function BoostPage() {
+  const [toastData, setToastData] = useState({ msg: "", icon: null, show: false });
+  const { data, setData, loading, setLoading } = useFetch();
+  const token = decrypted("token");
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${import.meta.env.VITE_API}api/users/status`, { headers: { Authorization: token } })
+      .then((res) => {
+        (res.data && setLoading(false)) || setData(res.data);
+      })
+      .catch((err) => {
+        (err && setLoading(false)) ||
+          setToastData({
+            icon: <XMarkIcon className="w-6 text-red-500" />,
+            msg: "Server Error",
+            show: true,
+          });
+      });
+  }, [setData, setLoading, token]);
+
+  return (
+    <>
+      <Toast icon={toastData.icon} msg={toastData.msg} show={toastData.show} setShow={setToastData} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="w-full h-svh flex flex-col">
+          <div className="flex-1 flex flex-col gap-5 items-center px-3 relative bg-primary pb-[100px]">
+            <BoostComponent />
+            <NavContainer />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
---
+++ src/pages/Boost/components/BoostComponent.jsx
@@ -0,0 +1,3 @@
+export default function BoostComponent() {
+  return <div>BoostComponent</div>;
+}

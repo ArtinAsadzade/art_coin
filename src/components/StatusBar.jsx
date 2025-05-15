8b8d483f265c1b1b2856abc388d5158768d4a544
ArtinAsadzade <artinasadzade7@gmail.com>
2025-05-15 16:35:28 +0330

    Update

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -3,9 +3,11 @@ import { UserAllDataContext } from "../context/UserAllDataContext";
 import Rank from "./Rank";
 import { Link } from "react-router-dom";
 import { ranks_url } from "../router/Urls";
+import { levelData } from "../data/Data";
 
 export default function StatusBar() {
-  const { allTokens, tokenLimit, perTap } = useContext(UserAllDataContext);
+  const { allTokens, tokenLimit, level } = useContext(UserAllDataContext);
+    const levelinfo = levelData.find(lvl => lvl.level === level)
   return (
     <div className="w-full flex flex-col gap-2">
       <div className="flex justify-between gap-2">
@@ -14,7 +16,7 @@ export default function StatusBar() {
         </div>
         <div className="w-full bg-secondary rounded-md p-2 flex justify-between font-bold text-primary">
           Per Tap:
-          <span>{perTap.toLocaleString("EN")}</span>
+          <span>{levelinfo?.perTap.toLocaleString("EN")}</span>
         </div>
       </div>
       <Link to={ranks_url} className="w-full bg-secondary rounded-md p-2 flex justify-center gap-2 font-bold text-primary">
